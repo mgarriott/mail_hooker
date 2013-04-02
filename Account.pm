7263bf3da7d901fe75fad1773e06a1dbeff260a9
Matt Garriott <matt.garriott@gmail.com>
2013-04-02 16:37:14 -0600

    Initial commit

---
+++ Account.pm
@@ -0,0 +1,78 @@
+package Account;
+1;
+
+use Mail::IMAPClient;
+use Time::HiRes;
+
+sub new {
+  $self = {};
+
+  my %hash = @_[1..$#_];
+
+  $self->{'imap'} = Mail::IMAPClient->new(%hash);
+  return bless $self;
+}
+
+sub fetch_mail_from_server {
+  $self = shift;
+  $self->{'imap'}->select('Inbox')
+    or die "Select '$Opt{folder}' error: ", $self->{'imap'}->LastError, "\n";
+
+  $self->{'mail'} = $self->{'imap'}->fetch_hash("FLAGS", "INTERNALDATE")
+    or die "Fetch mail '$Opt{folder}' error: ", $self->{'imap'}->LastError, "\n";
+
+  $self->{'imap'}->logout
+    or die "Logout error: ", $self->{'imap'}->LastError, "\n";
+}
+
+sub fetch {
+  $self = shift;
+  $self->fetch_mail_from_server;
+  $self->{'last_updated'} = time();
+}
+
+# Return true if the current mail information is outdated, i.e. more than 60
+# seconds old.
+sub outdated {
+  $self = shift;
+
+  if (defined $self->{'last_updated'}) {
+    $time_elapsed = time() - $self->{'last_updated'};
+    if ($time_elapsed >= 60) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else {
+    return 1;
+  }
+}
+
+# Return true if this account has new mail
+sub has_new_mail {
+  $self = shift;
+
+  if ($self->outdated) {
+    $self->fetch;
+  }
+  # $self->fetch if ($self->outdated);
+
+  my $result = 0;
+
+  foreach(values($self->{'mail'})) {
+    if ($$_{"FLAGS"} eq '') {
+      $result = 1;
+      last;
+    }
+  }
+
+  return $result;
+}
+
+# Return all the message ids
+sub get_message_ids {
+  $self = shift;
+
+  $keys = ['6999', '7001', '7002'];
+  return $keys;
+}
---
+++ test/test_new_email_notifier.pl
@@ -0,0 +1,67 @@
+use strict;
+use FindBin;                 # locate this script
+use lib "$FindBin::Bin/..";  # use the parent directory
+use Account;
+
+use Test::More tests => 12;
+
+my $acct = new Account();
+
+my %fake_mail = (
+      '6999' => {
+                  'INTERNALDATE' => '27-Feb-2013 01:45:40 +0000',
+                  'FLAGS' => '\\Seen'
+                },
+      '7001' => {
+                  'INTERNALDATE' => '27-Feb-2013 02:18:56 +0000',
+                  'FLAGS' => '\\Seen'
+                }
+    );
+
+package Account {
+  # Redefine the method that actually retrieves mail from the server. And
+  # provide something a bit more controlled.
+  sub fetch_mail_from_server {
+    $acct->{'mail'} = \%fake_mail;
+  };
+}
+
+sub outdate_account {
+  my $func = $_[0];
+
+  my $time_holder = $acct->{'last_updated'};
+  $acct->{'last_updated'} = time() - 61;
+  &$func();
+  $acct->{'last_updated'} = $time_holder;
+}
+
+ok(defined $acct,           'new() returned something');
+ok($acct->isa('Account'),   ' and it is the right class');
+
+# Account is outdated when first initialized
+ok($acct->outdated, 'Account\'s mail is outdated');
+
+# Fetch sets account as up-to-date
+$acct->fetch;
+ok(!$acct->outdated, 'Account\'s mail is up-to-date');
+
+# Elapsed time greater than 60 seconds flags account as out of date.
+outdate_account(sub { ok($acct->outdated, 'Account is outdated'); });
+
+ok(!$acct->has_new_mail, 'Account does NOT have new mail');
+
+$fake_mail{'7002'} = {
+                    'INTERNALDATE' => '27-Feb-2013 02:25:56 +0000',
+                    'FLAGS' => ''
+                  };
+
+ok($acct->has_new_mail, 'Account does have new mail');
+
+outdate_account(sub { ok($acct->has_new_mail,
+      'has_new_mail() fetches if outdated'); });
+
+my $ids = $acct->get_message_ids;
+is(@$ids, 3, 'get_message_ids returns list of 3 items');
+ok('6999' ~~ $ids, 'get_message_ids contains 6999');
+ok('7001' ~~ $ids, 'get_message_ids contains 7001');
+ok('7002' ~~ $ids, 'get_message_ids contains 7002');
