c7554bde4e47e73abb242e444e330ca4aa89c116
Matt Garriott <matt.garriott@gmail.com>
2013-04-03 22:17:58 -0600

    Add notifier.pl

---
+++ notifier.pl
@@ -0,0 +1,29 @@
+use FindBin;                # locate this script
+use lib "$FindBin::Bin/.";  # use the parent directory
+use Account;
+use YAML;
+
+my $input = '';
+while (<STDIN>) { $input .= $_ }
+
+my $config = Load($input);
+
+my $acct = new Account(
+  Server   => 'imap.gmail.com',
+  User     => $$config{'user'},
+  Password => $$config{'password'},
+  Ssl      => 1,
+  Uid      => 1,
+);
+
+$acct->fetch;
+$acct->register_seen;
+
+while (kill(0, $ARGV[0])) { # While the mutt process is still running...
+  $acct->fetch;
+  if ($acct->has_unseen) {
+    system('aplay ~/bin/finished.wav 2&> /dev/null');
+  }
+  $acct->register_seen;
+  sleep($$config{'refresh_time'});
+}
