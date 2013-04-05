f2ba4301fb729387ce91f748d7db7443c43af26d
Matt Garriott <matt.garriott@gmail.com>
2013-04-05 05:32:12 -0600

    Retrieve sound file from config file

--- new_email_notifier.pl
+++ new_email_notifier.pl
@@ -16,10 +16,32 @@ my $acct = new Account(
 $acct->fetch;
 $acct->register_seen;
 
-while (kill(0, $ARGV[0])) { # While the mutt process is still running...
+my $cmd;
+-e glob($$config{'sound_file'}) ||
+  die 'Sound file does not seem to exist. Check config.yml';
+if ($$config{'sound_file'} =~ /\.(wav|flac)$/) {
+  $cmd = 'aplay';
+} elsif ($$config{'sound_file'} =~ /\.mp3$/) {
+  $cmd = 'mpg123';
+} else {
+  $cmd = 'ogg123';
+}
+
+# if a command line arg was passed we'll use it as the mutt ID and kill the
+# script when that id is no-longer running. Otherwise we will run until
+# specifically killed.
+my $keep_running;
+if ($ARGV[0]) {
+  # Checks if the mutt process is still running
+  $keep_running = sub { kill(0, $ARGV[0]); };
+} else {
+  $keep_running = sub { return 1; };
+}
+
+while (&$keep_running()) {
   $acct->fetch;
   if ($acct->has_unseen) {
-    system('aplay ~/bin/finished.wav 2&> /dev/null');
+    system("$cmd $$config{'sound_file'} 2&> /dev/null");
   }
   $acct->register_seen;
   sleep($$config{'refresh_time'});
