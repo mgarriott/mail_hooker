2f0da1cdd23dcbae0e4f20805dff57060e6f89b7
Matt Garriott <matt.garriott@gmail.com>
2013-12-08 06:30:46 -0700

    Run specified command when there is new mail

--- src/new_email_notifier.pl
+++ src/new_email_notifier.pl
@@ -1,8 +1,9 @@
+use strict;
+use warnings;
 use FindBin;                # locate this script
 use lib "$FindBin::Bin/.";  # use the parent directory
 use Account;
 use YAML ('LoadFile', 'Load');
-
 
 my $config;
 if ($ARGV[1]) {
@@ -22,23 +23,15 @@ my $acct = new Account(
 $acct->fetch;
 $acct->register_seen;
 
-my $cmd;
--e glob($$config{'sound_file'}) ||
-  die "Sound file does not seem to exist. Check provided config info.";
-if ($$config{'sound_file'} =~ /\.(wav|flac)$/) {
-  $cmd = 'aplay';
-} elsif ($$config{'sound_file'} =~ /\.mp3$/) {
-  $cmd = 'mpg123';
-} else {
-  $cmd = 'ogg123';
-}
+$$config{'command'} ||
+  die "No command provided. Please check your config info.";
 
-# if a command line arg was passed we'll use it as the mutt ID and kill the
+# if a command line arg was passed we'll use it as the process ID and kill the
 # script when that id is no-longer running. Otherwise we will run until
-# specifically killed.
+# explicitly killed.
 my $keep_running;
 if ($ARGV[0]) {
-  # Checks if the mutt process is still running
+  # Checks if the process is still running
   $keep_running = sub { kill(0, $ARGV[0]); };
 } else {
   $keep_running = sub { return 1; };
@@ -47,7 +40,7 @@ if ($ARGV[0]) {
 while (&$keep_running()) {
   $acct->fetch;
   if ($acct->has_unseen) {
-    system("$cmd $$config{'sound_file'} 2&> /dev/null");
+    system($$config{'command'});
   }
   $acct->register_seen;
   sleep($$config{'refresh_time'});
